fd76da8ca7bbead19ffc3bc7a1588b1350d88d7a
Botmasher <languagejosh@hotmail.com>
2018-12-14 06:21:44 -0800

    Identify and store common letters per phone in word data

--- main.py
+++ main.py
@@ -13,9 +13,7 @@ def parse_dict_words(raw_text):
     #   - phones are already segmented
     #   - try guessing spelling associations starting with one-letter split
     lines = raw_text.split('\n')
-    print(len(lines))
     words = []
-    counter = 0
     for line in lines:
         if len(line) and re.match(r'[A-Za-z]', line[0]):
             segments = line.split()
@@ -25,15 +23,10 @@ def parse_dict_words(raw_text):
                 continue
             spelling_cleaned = re.split(r'\(', spelling)[0]
             sounds = [re.split(r'[0-9]', segment)[0] for segment in segments[1:]]
-            words.append([spelling_cleaned, sounds])
-            counter += 1
-        if counter == 20:
-            print(words)
-        continue
-    print(len(words))
+            words.append((spelling_cleaned, sounds))
     return words
 
-    # NOTE:
+    # NOTE: filter for "real" words
     # parsed words raise questions, like what to do about
     #   - AAA read as "triple A"
     #   - words with punctuation (hyphens, apostrophes, ...)
@@ -41,7 +34,22 @@ def parse_dict_words(raw_text):
     #   - personal names
     #   - capitalization
 
-def compare_sound_spelling(word_phones, word_letters):
+def compare_sound_spelling(words_letters_vs_phones):
+    """Compare distance between orthography and phonology in a set of words
+
+    input shape:    [word_0, ... word_n]
+                    where word_i = ('spelling', (phone_0, phone_1... phone_n))
+
+    calc shape:     {
+                        'phone': {
+                            'letter': 1,
+                            ...
+                        },
+                        ...
+                    }
+
+    return =>       0.0 <= distance_value <= 1.0
+    """
     # start from sounds and guess mapping letters to sounds
     # - consider place in word and try to assign each letter to one or more sounds
     # - could one sound be silence?
@@ -49,7 +57,29 @@ def compare_sound_spelling(word_phones, word_letters):
     # - or try naive scrambled version mapping sounds across the set
     #   - regardless of place?
     #   - for this you need to build and adjust data across words
-    return
+    letters_present_per_sound = {}
+    for word_data in words_letters_vs_phones:
+        letters = word_data[0]
+        sounds = word_data[1]
+        for sound in sounds:
+            if sound not in letters_present_per_sound:
+                letters_present_per_sound[sound] = {}
+            for letter in letters:
+                if letter in letters_present_per_sound[sound]:
+                    letters_present_per_sound[sound][letter] += 1
+                else:
+                    letters_present_per_sound[sound][letter] = 1
+    print(letters_present_per_sound)
+    letter_guesses_per_phone = {}
+    for sound in letters_present_per_sound:
+        sorted_letters = sorted(letters_present_per_sound[sound].items(), key=lambda k: k[1])
+        letter_guesses_per_phone[sound] = {}
+        # sorted letters contain letter count tuple pairs [('letter', n), ...]
+        letter_guesses_per_phone[sound]['graph'] = sorted_letters[0][0]
+        letter_guesses_per_phone[sound]['digraph'] = sorted_letters[1][0] if len(sorted_letters) > 1 else ''
+        letter_guesses_per_phone[sound]['trigraph'] = sorted_letters[2][0] if len(sorted_letters) > 2 else ''
+    # TODO: how to count spelling variants once possible graphs identified
+    return 0
 
 def open_file(url=""):
     if not url:
@@ -67,8 +97,10 @@ def start(dict_url=""):
     dict_txt = open_file(url=dict_url)
     if not dict_txt or type(dict_txt) != str:
         return
-    words = parse_dict_words(dict_txt)
-    return 1.0
+    words_spellings_sounds = parse_dict_words(dict_txt)
+    print(words_spellings_sounds[:5])
+    distance = compare_sound_spelling(words_spellings_sounds)
+    return distance
 
 if __name__ == '__main__':
     phones_dict_url = "http://svn.code.sf.net/p/cmusphinx/code/trunk/cmudict/cmudict-0.7b"
