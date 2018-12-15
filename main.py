8146de8691d5e23a837f168310dd82c6cc14f9e7
Botmasher <languagejosh@hotmail.com>
2018-12-15 10:51:28 -0800

    Tally letter variants, calculate average variation and take comments on likely issues

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 # #!/usr/bin/env python
 import requests
 import re
-#import asyncio
 
 def parse_dict_words(raw_text):
     # break down input and return array of words
@@ -34,19 +33,26 @@ def parse_dict_words(raw_text):
     #   - personal names
     #   - capitalization
 
-def compare_sound_spelling(words_letters_vs_phones):
+def compare_sound_spelling(words_letters_vs_phones, sensitivity=0.1):
     """Compare distance between orthography and phonology in a set of words
 
-    input shape:    [word_0, ... word_n]
-                    where word_i = ('spelling', (phone_0, phone_1... phone_n))
+    inputs:
+        words_letters_vs_phones = [word_0, ... word_n]
+        # where each word = ('spelling', ('phone_0', ... 'phone_n'))
 
-    calc shape:     {
-                        'phone': {
-                            'letter': 1,
+        0.0 <= sensitivity <= 1.0
+        # used to find letter variant likelihood
+
+    count shape:    {
+                        'phone': {          # each found phone
+                            'letter': 1,    # count each letter occurrence in every word containing phone
                             ...
                         },
                         ...
                     }
+    # then use mapped counts and sensitivity to guess associating letters to each phone
+
+    # TODO: add considering di- and trigraphs during count map
 
     return =>       0.0 <= distance_value <= 1.0
     """
@@ -58,6 +64,33 @@ def compare_sound_spelling(words_letters_vs_phones):
     #   - regardless of place?
     #   - for this you need to build and adjust data across words
     letters_present_per_sound = {}
+
+    # TODO: understand and experiment with sensitivity
+    sensitivity # clamp 0.0 <= s <= 1.0
+
+    # NOTE: building letters per sound
+    # - figure out which letters are present in words that have a sound
+    # - assumes that certain sounds will emerge
+    #
+    # mock example:
+    # - input: 'CAT', 'SAD', 'ADD', 'AFTER', 'RAN', 'PASS', 'AS'
+    # - expected: associate the sound "/æ/" with the letter "A"
+    # - input: 'QUILT', 'CALL', 'CAT', 'CORE', 'KEEP', 'SACK', 'PLACATE', 'SICKER', 'UNIQUE'
+    # - expected: associate the sound "/k/" with the letters "C", "K", ("Q(U)"?)
+    #
+    # uncertainties & issues:
+    # - aren't you assuming that letter-sound cooccurrences are noise apart from letter-phone correspondences?
+    #   - nonrandom: 'U' after 'Q' (? or expect this as a digraph)
+    #   - nonrandom: 'E' is in so many spellings; '/ə/' is such a common phoneme
+    # - how does this handle very common sounds?
+    #   - think about all of the possible variants for "/ə/"
+    # - how does this handle rare letters or rare sounds?
+    #   - including less common sounds like EN "/ʒ/" or even "/θ/"
+    #   - including rare letter-sound cooccurrences like "zh" for
+    # - should we figure Zipf's before tying sounds to letters?
+    # - are you able to map both ways, from letters->sounds and sounds->letters?
+    #   - GK iotacism leaves one sound -> many letters
+    #   - EN vowel shift leave multiple sounds <- per letter
     for word_data in words_letters_vs_phones:
         letters = word_data[0]
         sounds = word_data[1]
@@ -69,17 +102,43 @@ def compare_sound_spelling(words_letters_vs_phones):
                     letters_present_per_sound[sound][letter] += 1
                 else:
                     letters_present_per_sound[sound][letter] = 1
-    print(letters_present_per_sound)
+
+    # TODO: split finding letter options away from finding polygraphs
+    # - map possible di- and trigraphs while iterating through individual words
+    # - determine letter variants per sound after all words traversed
+
+    # sort letters per sound by frequency and choose the most common candidates depending on sensitivity
     letter_guesses_per_phone = {}
     for sound in letters_present_per_sound:
         sorted_letters = sorted(letters_present_per_sound[sound].items(), key=lambda k: k[1])
-        letter_guesses_per_phone[sound] = {}
+
         # sorted letters contain letter count tuple pairs [('letter', n), ...]
-        letter_guesses_per_phone[sound]['graph'] = sorted_letters[0][0]
-        letter_guesses_per_phone[sound]['digraph'] = sorted_letters[1][0] if len(sorted_letters) > 1 else ''
-        letter_guesses_per_phone[sound]['trigraph'] = sorted_letters[2][0] if len(sorted_letters) > 2 else ''
-    # TODO: how to count spelling variants once possible graphs identified
-    return 0
+
+        # default guess is the most common letter
+        letter_guesses_per_phone[sound] = [sorted_letters[0][0]]
+        highest_letter_count = sorted_letters[0][1]
+
+        # variant guesses based on sensitivity
+        for letter_and_count in sorted_letters[1:]:
+            letter = letter_and_count[0]
+            tally = letter_and_count[1]
+            if tally >= highest_letter_count - (sensitivity * highest_letter_count):
+                letter_guesses_per_phone[sound].append(letter)
+            else:
+                # letters are sorted by frequency - end loop once reach first nonvariant
+                break
+        continue
+
+    # count variants per letter and propose an average distance
+    average_distance = 0.0
+    variants_total = 0
+    # determine average number of letters per sound
+    for variants in letter_guesses_per_phone.values():
+        variants_total += len(variants)     # calc above during prev loop?
+    average_variants = variants_total / len(letter_guesses_per_phone.keys())
+
+    print(average_variants)
+    return average_variants
 
 def open_file(url=""):
     if not url:
